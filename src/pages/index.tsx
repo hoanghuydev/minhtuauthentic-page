d434f2ade95109b6905a77cac0894e590efef871
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-06-15 21:20:50 +0700

    add home schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,8 +43,89 @@ export default function Home({
   homePage: ResponseHomePageDto;
   settingsHome: Record<string, SettingOptionDto | undefined>;
 } & PageSetting) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "Store",
+    "name": "Minh Tu Authentic",
+    "image": "https://be.minhtuauthentic.com/public/Logo%20Thuong%20hieu/logo%20chinh%202070x540%2001.png",
+    "url": "https://minhtuauthentic.com/",
+    "telephone": "0961693869 ",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "278 Hòa Bình, Q Tân Phú, TpHCM",
+      "addressLocality": "TpHCM",
+      "addressRegion": "Quận Tân Phú",
+      "postalCode": "72000",
+      "addressCountry": "Viet Nam"
+    },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": "10.7726003",
+      "longitude": "106.6267201,17"
+    },
+    "openingHoursSpecification": [
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Monday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Tuesday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Wednesday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Thursday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Friday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Saturday",
+        "opens": "09:00",
+        "closes": "21:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Sunday",
+        "opens": "09:00",
+        "closes": "21:00"
+      }
+    ],
+    "sameAs": [
+      "https://www.facebook.com/minhtuauthentic/",
+      "https://x.com/MinhTuAuthentic",
+      "www.youtube.com/@MinhTuAuthentic",
+      "https://www.reddit.com/user/minhtuauthentic/",
+      "https://www.quora.com/profile/MinhTuAuthentic",
+      "https://medium.com/@minhtuauthentic/minh-tú-authentic-chuyên-phân-phối-nước-hoa-chính-hãng-từ-các-thương-hiệu-uy-tín-toàn-cầu-404736b10787",
+      "https://minhtuauthenticvn.blogspot.com/2025/05/minh-tu-authentic-chuyen-phan-phoi-nuoc.html"
+      ,
+      "https://www.linkedin.com/in/minh-tu-authentic-b56732364/"
+    ]
+  }
   return (
     <>
+      <Head>
+        <script type="application/ld+json">
+          {JSON.stringify(schema)}
+        </script>
+      </Head>
       <Header settings={settings} menu={menu} />
       <HomeBanner
         setting={settingsHome[SETTING_KEY.BANNER_SECTION.KEY]}
