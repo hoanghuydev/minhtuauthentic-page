5eafbad08fb94ec7d6d7dbadfcf79b5bb0022a26
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-07-18 18:35:21 +0700

    add home support to product detail page

--- src/dtos/responseProductDetailPage.dto.ts
+++ src/dtos/responseProductDetailPage.dto.ts
@@ -3,6 +3,7 @@ import { ProductConfigurationsDto } from '@/dtos/productConfigurations.dto';
 import { SettingsDto } from '@/dtos/Settings.dto';
 import { PromotionsDto } from '@/dtos/Promotions.dto';
 import { VariantDto } from '@/dtos/Variant.dto';
+import { StaticContentsDto } from '@/dtos/StaticContents.dto';
 
 export class ResponseProductDetailPageDto {
   product?: ProductDto;
@@ -10,5 +11,7 @@ export class ResponseProductDetailPageDto {
   productConfigurations?: ProductConfigurationsDto[];
   promotionsProducts?: PromotionsDto;
   settings?: SettingsDto[];
+  settingsHome?: SettingsDto[];
   variantActive?: VariantDto;
+  homeSupport?: StaticContentsDto[];
 }
