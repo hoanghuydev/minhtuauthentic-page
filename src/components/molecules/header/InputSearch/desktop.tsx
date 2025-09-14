2a8606176bf87a31b1a11b271afc895a30164552
quangtho908 <quangtho23062002@gmail.com>
2025-09-15 00:59:41 +0700

    fix body style

--- src/components/molecules/header/InputSearch/desktop.tsx
+++ src/components/molecules/header/InputSearch/desktop.tsx
@@ -55,7 +55,7 @@ export const InputSearchDesktop = ({ classname, isForMobile }: Props) => {
     if (ctx?.isOpenSearch && isMobile) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflowX = 'hidden';
+      document.body.style.overflow = 'auto';
     }
   }, [ctx?.isOpenSearch, isMobile]);
 
